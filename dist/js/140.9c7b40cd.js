f4cda1fc8dd1d0d8499f33b9edba20ccd83ad0ee
Michell Pacheco Tacay <mpacheco.tacay@gmail.com>
2023-12-27 17:16:31 -0500

    se esta habilitando la carpeta dist

---
+++ dist/js/140.9c7b40cd.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkplatform_electronic_billing"]=self["webpackChunkplatform_electronic_billing"]||[]).push([[140],{140:function(n,e,l){l.r(e),l.d(e,{default:function(){return f}});var r=l(3396);const t={class:"p-5"},c=(0,r._)("h1",null,"Estamos en el home",-1),i=[c];function o(n,e){return(0,r.wg)(),(0,r.iD)("div",t,i)}var s=l(89);const u={},a=(0,s.Z)(u,[["render",o]]);var f=a}}]);
+//# sourceMappingURL=140.9c7b40cd.js.map
